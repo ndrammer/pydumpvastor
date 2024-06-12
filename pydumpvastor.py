deddad64626baa598cefaca9ca341ce8fb4a69d1
ndrammer <153938682+ndrammer@users.noreply.github.com>
2024-06-12 17:23:14 +0200

    Update pydumpvastor.py

--- pydumpvastor.py
+++ pydumpvastor.py
@@ -1,4 +1,4 @@
-(import ctypes
+import ctypes
 import ctypes.wintypes
 from ctypes.wintypes import WORD, DWORD,LONG, LPVOID, HANDLE
 from ctypes import byref
@@ -9,30 +9,32 @@ import io
 import argparse
 
 
-#argument parsing
-parser = argparse.ArgumentParser(description='Memory Process Dumpo')
+
+#------Argument parsing------
+parser = argparse.ArgumentParser(description='Process Memory Dump')
 parser.add_argument('pid', help='process PID')
 args = parser.parse_args()
 
-#pid
+#------pid-----------
 pid = int(args.pid)  
 
-#Constants
-PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
+#------Constants-----
+PROCESS_ALL_ACCESS = 0x1f0fff
+PROCESS_VM_READ = 0x0010
 
 
-# Types definitions
+#------Types definitions----
 SIZE_T = ctypes.c_size_t
 if ctypes.sizeof(ctypes.c_void_p) == ctypes.sizeof(ctypes.c_ulonglong):
     DWORD_PTR = ctypes.c_ulonglong
 elif ctypes.sizeof(ctypes.c_void_p) == ctypes.sizeof(ctypes.c_ulong):
     DWORD_PTR = ctypes.c_ulong
 
-#Open process
-handle = ctypes.windll.kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
+#-----Get process handle------
+handle = ctypes.windll.kernel32.OpenProcess(PROCESS_ALL_ACCESS or PROCESS_VM_READ, False, pid)
 
 
-#System info
+#-----System & Memory info------
 class SYSTEM_INFO(ctypes.Structure):
     _fields_ = [
         ("wProcessorArchitecture", ctypes.c_ushort),
@@ -59,70 +61,60 @@ sysinfo = SYSTEM_INFO()
 Kernel32.GetSystemInfo(ctypes.byref(sysinfo))
 
 
-#baseaddress
-hModule = ctypes.c_ulonglong()  #  c_ulong for 32 bits
-count = ctypes.c_ulong()
-modname = ctypes.create_unicode_buffer(30)
-    
-# Enum Process
-if ctypes.windll.psapi.EnumProcessModulesEx(handle, ctypes.byref(hModule), ctypes.sizeof(hModule), ctypes.byref(count), 3):
-
-    print(f"Process BaseAddress: {hex(hModule.value)}")
-else:
-    print("Not possible to get BaseAddress.")
-
 
 class MEMORY_BASIC_INFORMATION(ctypes.Structure):
-    _fields_ = (('BaseAddress', ctypes.c_uint64),  #LPVOID for 32bits
-                ('AllocationBase',    ctypes.c_uint64),  #LPVOID  for 32bits
+    _fields_ = (('BaseAddress', ctypes.c_ulonglong), 
+                ('AllocationBase',    ctypes.c_ulonglong),  
                 ('AllocationProtect', DWORD),
-                ('RegionSize', ctypes.c_uint64),  #SIZE_T  for 32bits
+                ("__alignment1", wintypes.DWORD),
+                ('RegionSize', ctypes.c_ulonglong),  
                 ('State',   DWORD),
                 ('Protect', DWORD),
-                ('Type',    DWORD))
-
-mbi = MEMORY_BASIC_INFORMATION()
-
-ctypes.windll.Kernel32.VirtualQueryEx(handle, ctypes.c_uint64(hModule.value), ctypes.byref(mbi),ctypes.sizeof(mbi)) 
+                ('Type',    DWORD),
+                ("__alignment2", wintypes.DWORD))
 
 
+mem_region_begin = sysinfo.lpMinimumApplicationAddress
+mem_region_end = sysinfo.lpMaximumApplicationAddress
+address = mem_region_begin
 
-#----------Dumping-------------------
-
-
-# Object BytesIO for dumping
+#-------Object BytesIO for dumping----
+chunk_size = 4096
 dump_data = io.BytesIO()
-
-address=mbi.BaseAddress
-
-buffer = (ctypes.c_char * 4096)()
-bytesRead = SIZE_T()
-blocks=(mbi.RegionSize/sysinfo.dwPageSize)+1
-final=address+blocks*sysinfo.dwPageSize
+total_size=0
 
 
+#-------Memory regions travelling and dumping-----
+while address < mem_region_end:
 
-while (address < final):
+    mbi = MEMORY_BASIC_INFORMATION()
 
-    if (ctypes.windll.kernel32.ReadProcessMemory(handle, ctypes.c_uint64(address), ctypes.byref(buffer), 4096, ctypes.byref(bytesRead))):
-        print('[...]Dumping')
-        dump_data.write(buffer.raw)
-    else:
-        ctypes.WinError(ctypes.get_last_error())
+    ctypes.windll.Kernel32.VirtualQueryEx(handle, ctypes.c_uint64(address), ctypes.byref(mbi),ctypes.sizeof(mbi)) 
+    if (mbi.Protect!=(0x01) and mbi.State==0x1000):        
+        print(f"BA:{hex(mbi.BaseAddress)} Size:{mbi.RegionSize/1024}")
+        total_size+=mbi.RegionSize
 
-    address += sysinfo.dwPageSize
+         #----------Dumping-------------------
+
+        for addres_s in range(mbi.BaseAddress, mbi.BaseAddress+mbi.RegionSize, chunk_size):
+            buffer = (ctypes.c_char * 4096)() 
+            #bytesRead = SIZE_T()                   
+            if (ctypes.windll.kernel32.ReadProcessMemory(handle, ctypes.c_ulonglong(addres_s), ctypes.byref(buffer), 4096, None)):
+                #print('[...]Dumping')
+                dump_data.write(buffer.raw)
+            else:
+                ctypes.WinError(ctypes.get_last_error())
+
+    address += mbi.RegionSize
     
-
 print('[+] Dump finished')
-#print(buffer.raw)
-
-#-----------Writing to file-----------
+print(f"Total Size (bytes): {total_size} (Kbytes): {total_size/1024} ")
 
 
+#-----------Closing process handle-----------
 ctypes.windll.kernel32.CloseHandle(handle)
 
-# Compress data
+#-----------Writing to file (zipped)-----------
 with zipfile.ZipFile('ex.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
-    #zipf.setpassword(b'S3cr3t')  # Password only works for reading in zipfile library
     dump_data.seek(0)  
     zipf.writestr('dump.bin', dump_data.read())  
